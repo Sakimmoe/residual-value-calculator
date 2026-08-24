91297d96c5405d4aee429ebaab7ef9ed8f5556dc
油咖喱 <140014170+Sakimmoe@users.noreply.github.com>
2026-08-24 14:35:02 +0800

    Delete worker.js

--- worker.js
+++
@@ -1,9 +0,0 @@
-export default {
-  async fetch(request, env) {
-    try {
-      return await env.ASSETS.fetch(request);
-    } catch (e) {
-      return new Response("Not Found", { status: 404 });
-    }
-  },
-};
